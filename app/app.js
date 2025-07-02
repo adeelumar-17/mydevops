f643eb898dc3f37c3ac404c9ad95ff8efaa5abf6
Adeel Umar <adeelumar30@hotmail.com>
2025-07-03 03:01:21 +0500

    update app.js

--- app/app.js
+++ app/app.js
@@ -4,4 +4,6 @@ require('dotenv').config();
 const app = express();
 app.get('/', (req, res) => res.send('🚀 Hello from Dockerized Node App!'));
 
-app.listen(process.env.PORT || 3000, () => console.log('App running...'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`App running on port ${PORT}...`));
+
