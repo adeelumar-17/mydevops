9a1cb1104a3caae8392eeeaef77e81d838a25adb
Adeel Umar <adeelumar30@hotmail.com>
2025-07-03 13:14:13 +0500

    update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-app.get('/', (req, res) => res.send('🚀 Hello from Dockerized Node App!'));
+app.get('/', (req, res) => res.send('🚀 Hello from FAST NUCES'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`App running on port ${PORT}...`));
